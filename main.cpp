3539ee6813b7112fc2f061ba98083204df5a84c0
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2025-12-17 08:21:38 +0000

    aggiunta printArray

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void printArray( int v[], int l  ) {
+    for (int i=0; i<l; i++) {
+        cout << v[i] << "\t";
+    }
+    cout << endl;
+}
+
 
 int main()
 {
-    cout<<"hello world"<< endl;
+    int vett[] = { 3, 23, -12, 145 };
 
+    printArray( vett, 4 );
 
     return 0;
 }
